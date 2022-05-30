3e4776e5df226afa0047d3f85cc0b9c7cd19f7b0
Harshini <praveenabhandary219@gmail.com>
2022-05-30 13:10:38 +0530

    Added a function to generate grid

--- Grid.js
+++ Grid.js
@@ -4,33 +4,39 @@ class Grid {
 
     }
 
+    getCell(cellNumber, isOdd) {
+        const cell = document.createElement('td');
+        cell.className = 'cell';
+        cell.id = 'Cell-' + cellNumber;
+        cell.textContent = cellNumber;
+
+        if (isOdd) cell.classList.add('odd');
+        else cell.classList.add('even');
+
+        return cell
+    }
+
     gridGenerator(numBox) {
         const gridContainer = document.querySelector('.grid-container');
 
         const grid = document.createElement('table');
         grid.className = 'grid';
         let counter = numBox * numBox;
-    
+
 
         for (let i = 0; i < numBox; i++) {
             let isOdd = i % 2;
-            let startingCount = counter;
 
+            let startingCount = counter;
             if (isOdd) {
                 counter = counter - numBox + 1;
             }
 
             const row = document.createElement('tr');
             row.className = 'row';
-            for (let j = 0; j < numBox; j++) {
-                const cell = document.createElement('td');
-                cell.className = 'cell';
-                cell.id = 'Cell-' + counter;
-                cell.textContent = counter;
-                row.appendChild(cell);
-                if (isOdd) cell.classList.add('odd');
-                else cell.classList.add('even');
 
+            for (let j = 0; j < numBox; j++) {
+                row.appendChild(this.getCell(counter, isOdd));
                 if (isOdd) counter++;
                 else counter--;
             }
